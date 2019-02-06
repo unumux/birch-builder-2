36b974f81d5f22593050ccf07aba874b49c69703
Ivan Town <itown@unum.com>
2019-02-06 12:46:11 -0500

    added 200dc to the alignedLeft filter category

--- main.js
+++ main.js
@@ -1250,7 +1250,7 @@ const megaTemplateObj = {
         </td></tr></tbody></table>
         <!-- /c200dc -->`,
         category: 'body',
-        tags: ['1col', 'alignedCenter'],
+        tags: ['1col', 'alignedCenter', 'alignedLeft'],
         defaultUnumDataObj: { // matches templateValuesObj on the data instance obj
             backgroundColor: '#f7f7f7',
             spacerTop: '50',
